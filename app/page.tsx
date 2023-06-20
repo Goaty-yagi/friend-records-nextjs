bc157505483d7e9ef707260b7bb7e79161e89570
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-06-20 13:31:46 +1000

    first commit.

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Image from 'next/image'
+import { Text } from '@chakra-ui/react'
+export default function Home() {
+  return (
+    <main>
+      <Text m={'2rem'}>HOME</Text>
+    </main>
+  )
+}
