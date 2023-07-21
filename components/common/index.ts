f92371b345cd7b57497be45e4f3b3c24ee98b0c1
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-07-22 00:09:17 +1000

    change spinner place.

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,8 +1,8 @@
 export { default as Navbar } from './navbar';
 export { default as MobileNavbar } from './mobileNavbar';
-export { default as Spinner } from './Spinner';
 export { default as Layout } from './layout';
 export { default as SocialButton } from './SocialButton';
 export { default as SocialButtons } from './SocialButtons';
 export { default as YScrollLimitationWrapper } from './YScrollLimitationWrapper';
 export { default as CustomImages } from './CustomImages';
+export { default as Spinner } from './Spinner';
