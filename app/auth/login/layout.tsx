9d01d0a9ad5db013955aca82b804e3fb6003a4cf
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-09-10 16:23:15 +1000

    add back to home in login and register.

--- app/auth/login/layout.tsx
+++ app/auth/login/layout.tsx
@@ -5,8 +5,11 @@ import { LoginForm } from "@/components/forms";
 import { SocialButtons } from "@/components/common";
 import { PrivateRouterWithAuth } from "@/components/common/PrivateRouter";
 import { Heading, Box, Text, Flex, Link } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { IoHomeOutline } from "react-icons/io5";
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
+  const router = useRouter();
   return (
     <>
       <PrivateRouterWithAuth>
@@ -23,6 +26,21 @@ export default function Layout({ children }: { children?: React.ReactNode }) {
                 Register here
               </Link>
             </Text>
+            <Box display={"inline-block"}>
+              <Flex
+                alignItems={"center"}
+                p={"0.5rem"}
+                transition={"300ms"}
+                borderRadius={"5px"}
+                cursor={"pointer"}
+                _hover={{ bg: "gray" }}
+              >
+                <IoHomeOutline />
+                <Text ml={"0.3rem"} onClick={() => router.push("/")}>
+                  Back to home
+                </Text>
+              </Flex>
+            </Box>
           </Box>
         </Flex>
       </PrivateRouterWithAuth>
--- app/auth/register/layout.tsx
+++ app/auth/register/layout.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { Heading, Flex, Box } from "@chakra-ui/react";
+import { Heading, Flex, Box, Text } from "@chakra-ui/react";
 import { PrivateRouterWithAuth } from "@/components/common/PrivateRouter";
 import { RegisterForm } from "@/components/forms";
+import { useRouter } from "next/navigation";
+import { IoHomeOutline } from "react-icons/io5";
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
+  const router = useRouter();
   return (
     <>
       <PrivateRouterWithAuth>
@@ -13,6 +16,21 @@ export default function Layout({ children }: { children?: React.ReactNode }) {
               Register
             </Heading>
             {<RegisterForm />}
+            <Box display={"inline-block"}>
+              <Flex
+                alignItems={"center"}
+                p={"0.5rem"}
+                transition={"300ms"}
+                borderRadius={"5px"}
+                cursor={"pointer"}
+                _hover={{ bg: "gray" }}
+              >
+                <IoHomeOutline />
+                <Text ml={"0.3rem"} onClick={() => router.push("/")}>
+                  Back to home
+                </Text>
+              </Flex>
+            </Box>
           </Box>
         </Flex>
       </PrivateRouterWithAuth>
