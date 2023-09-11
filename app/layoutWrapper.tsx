d3b15fd36605b8bb302830d2f1302e51e09c78fa
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-09-11 20:35:13 +1000

    add svh.

--- app/layoutWrapper.tsx
+++ app/layoutWrapper.tsx
@@ -15,24 +15,25 @@ interface Props {
 export default function LayoutWrapper({ children }: Props) {
   const { colorMode, toggleColorMode } = useColorMode();
   const { theme } = useThemeColors("bg");
-  const [h, setH] = useState(0)
-  useEffect(() => {
-    function setHeight() {
-      const vh = window.innerHeight * 0.01
-      setH(vh * 100 )
-    }
+  // const [h, setH] = useState(0)
+  // useEffect(() => {
+  //   function setHeight() {
+  //     const vh = window.innerHeight * 0.01
+  //     setH(vh * 100 )
+  //   }
     
-    setHeight();
-    window.addEventListener('resize', setHeight);
-    return () => window.removeEventListener('resize', setHeight);
-  },[])
+  //   setHeight();
+  //   window.addEventListener('resize', setHeight);
+  //   return () => window.removeEventListener('resize', setHeight);
+  // },[])
   const { data: user, isLoading } = useRetrieveUserQuery();
   const { isAuthenticated } = useAppSelector((state) => state.auth);
   return (
     <Box
       position={"absolute"}
       w={"100vw"}
-      minH={h?h:"100vh"}
+      // minH={h?h:"100vh"}
+      minH={"100svh"}
       top={0}
       pt={{ base: 0, md: "80px" }}
       bg={isAuthenticated ? 'url("/images/background.png")' : ""}
