db1f8fc81bb583dfd94a413828aacb65dd83cde3
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-09-13 23:06:33 +1000

    add style for not-found page.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,34 @@
-'use client'
+"use client";
 
-import { Text } from '@chakra-ui/react'
-export default function Home() {
+import {
+  Text,
+  Heading,
+  Divider,
+  Center,
+  Button,
+  Image
+} from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import Logo from "@/public/logo.svg";
+
+export default function NotFound() {
+  const router = useRouter();
   return (
-    <main>
-      <Text m={'2rem'}>Not Found</Text>
-    </main>
-  )
+    <Center w={'100%'} mt={"6rem"} alignItems={'center'} position={'relative'} left={"-1rem"}>
+      <Image as={Image} src={Logo.src} alt="logo" height="50px" m={"0 0.8rem"} />
+      <Center flexDirection={"column"} alignItems={"center"}>
+        <Heading as="h1">Not found</Heading>
+        <Text>The page you&apos;re looking for was not found.</Text>
+        <Divider my={6} />
+        <Button
+          onClick={() => {
+            router.push("/");
+          }}
+          colorScheme="teal"
+        >
+          Return to home
+        </Button>
+      </Center>
+    </Center>
+  );
 }
